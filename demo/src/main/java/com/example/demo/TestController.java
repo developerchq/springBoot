dc201a20cdfd53b011fd0ab6e10b1247218bfcbf
developerchq@gmail.com <developerchq@gmail.com>
2018-11-29 10:21:28 +0800

    init

---
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+@RequestMapping("/sample")
+public class TestController {
+	
+	@RequestMapping("/home")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+}
