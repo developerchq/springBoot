71b681e14bb2121a8b0520cea3522589f29395a1
developerchq@gmail.com <developerchq@gmail.com>
2018-12-10 19:58:40 +0800

    add .properties和.yml配置文件

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude=DataSourceAutoConfiguration.class)
-public class DemoApplication {
 
+@SpringBootApplication //(exclude=DataSourceAutoConfiguration.class)
+public class DemoApplication implements CommandLineRunner{
+
+	@Autowired
+    DataSource dataSource;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	/**
+	 * 查看用了什么数据库连接池
+	 */
+	public void run(String... args) throws Exception {
+        System.out.println("DATASOURCE = " + dataSource);
 	}
 }
